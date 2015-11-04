7b82667e002ec4469fd2fda86dde68b598770003
Cynthia Salinas <csalinas123@gmail.com>
2015-11-04 08:57:20 -0800

    Wk2Day2

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 // wait for DOM to load before running JS
 $(function() {
-
+ // 1. Create a Form
+ // 2. make AJAx request when form submits
+ // 3. Get user input from form to send to spotify
   // check to make sure JS is loaded
-  console.log('JS is loaded!');
-  
-  // your code here
 
-});
+	//Submit the form to search spotfiy API
+	$('#search').on('submit', function(event){
+  	event.preventDefault(); 
+
+    // reset the form
+    $(tracks).empty();
+
+	//save the form entries to variable
+	var searchedTrack = $('#searchedTrack').val();
+
+	// Tracked results that are in the array called 'items'
+	// The array is nested in 'tracks' 
+	$.get('https://api.spotify.com/v1/search?q=' + searchedTrack + '&type=track', function (data){
+  	var artistResults = data.tracks.items;
+  	console.log(artistResults); 
+
+	//appended the tracks to view on window 
+  	var artistSongs = artistResults.forEach(function(track){
+  		console.log(track.name); 
+  		$('#results').append('<p>' + track.name + '</p>');	
+
+
+	//empty - clear the hello results and put good bye results 
+  	// console.log('JS is loaded!');
+	 });
+});  
+}); 
+}); 
